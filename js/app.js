db4d20b14730b4e47e7853f45ed7f1995df6f3e0
Eduardo <hemogro@gmail.com>
2020-06-01 18:04:34 -0300

    App.js agregado

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+
+	el: "#root",
+	data: {
+
+    },
+
+    mounted: function () {
+        console.log("Vue running...");
+	},
+
+	methods: {
+
+    }
+});
+Vue.config.devtools = true;
