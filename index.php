8738039550e2ff5caaaeaf34befd1185d0c658ac
Oscar <43421688+dert98@users.noreply.github.com>
2020-06-01 18:49:20 -0300

    modifiq el v1.php de la api

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <link rel="stylesheet" href="css/styles.css">
-<!-- carpeta de imagen    /images -->
+<!-- carpeta de img    /img -->
 <head>
 	<title></title>
 </head>
-<body>   
+<body>
 
 	<h1 class="bg-primary">Hola mariano</h1>
 	<!-- Navbar -->
